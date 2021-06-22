690a649b5adf6a8f6555da9bd240e51fcc482edd
ttippens <49510050+ttippens@users.noreply.github.com>
2021-06-22 15:33:03 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    return df
 
 def clean_data(df):
-    pass
-
+    
+    categories = df['categories'].str.split(';',expand=True)    
+    row = categories.iloc[0]    
+    category_colnames = row.apply(lambda x: x[:len(x) - 2]) 
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    df.drop('categories', inplace=True, axis=1)
+    df = pd.concat([df, categories], axis=1, join='inner', sort=False)
+    df_duplicates = df[df.duplicated()]
+    df.drop(df_duplicates.index, inplace = True)
+    df_related_2 = df[df['related'] == 2]
+    df.drop(df_related_2.index,inplace = True) 
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('etl_clean', engine, index=False,if_exists = 'replace')
 
 
 def main():
